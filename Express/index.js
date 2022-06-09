eae18559729e60ef3f06a7be36621e8c8352c99d
Tyron Pyle <tyron.pyle@gmail.com>
2022-06-09 10:30:39 +0100

    update

--- Express/index.js
+++ Express/index.js
@@ -2,23 +2,15 @@
 const express = require ("express");
 
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const nameRouter = require('./router/names');
 
 const server = app.listen(4494,() =>
 {
     console.log(`server started succesfully on port number ${server.address().port}`)
 });
-
-
-
-app.get('/hello', (req, res)=> res.send("Hello world"));
-
-app.post('/create', (req, res) => console.log('create'));
-
-app.put('/replace', (req, res) => console.log('replace'));
-
-app.patch('/update', (req, res) => console.log('update'));
-
-app.delete('/delete', (req, res) => console.log('delete'));
 
 let barry = {
     "id": 1,
@@ -26,24 +18,6 @@ let barry = {
     "age": 44,
     "job": "Receptionist"
 }
-
-let name = ["Tyron","Michal","Callum","Nikita","Molly"]
-
-app.get('/getAll', (req, res) => res.send (name));
-
-app.get('/get/:id', (req, res) => res.send(name[req.params.id,0]));
-
-app.get('/delete/:id', (req, res) => {
-    res.send(name.splice(req.params.id, 1));
-});
-
-app.use(express.json());
-
-app.post('/create', (req, res) => {
-    const nameAdd = req.body.name;
-    names.push(nameAdd);
-    res.status(201).send(`${nameAdd} added successfully`);
-});
 
 const logger = (req, res, next) => {
     console.log(new Date());
@@ -54,13 +28,11 @@ app.get('/', logger, (req, res) => {
     res.send('Hello, world!');
 });
 
+
+app.use('*', (req, res, next) => next({ status: 404, message: 'Invalid url' })); // catches 404's
 //test to see if it works
 
 app.use((err, req, res, next) => {
     res.status(err.status ? err.status : 500).send(err.message);
-  });
-  
-  const server = app.listen(4494, () => {
-    console.log('Server started on', server.address().port);
   });
   
---
+++ Express/router/names.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+
+let name = ["Tyron","Michal","Callum","Nikita","Molly"]
+
+
+router.get('/hello', (req, res)=> res.send("Hello world"));
+
+router.post('/create', (req, res) => console.log('create'));
+
+router.put('/replace', (req, res) => console.log('replace'));
+
+router.patch('/update', (req, res) => console.log('update'));
+
+router.delete('/delete', (req, res) => console.log('delete'));
+
+router.get('/getAll', (req, res) => res.send (name));
+
+router.get('/get/:id', (req, res) => res.send(name[req.params.id,0]));
+
+router.get('/delete/:id', (req, res) => {
+    res.send(name.splice(req.params.id, 1));
+});
+
+router.use(express.json());
+
+router.post('/create', (req, res) => {
+    const nameAdd = req.body.name;
+    names.push(nameAdd);
+    res.status(201).send(`${nameAdd} added successfully`);
+});
+
+module.exports = router;
