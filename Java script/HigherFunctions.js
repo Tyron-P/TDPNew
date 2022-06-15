a1678b52b3d77a0ff7fce905fe97dfd85ad21985
Tyron Pyle <tyron.pyle@gmail.com>
2022-06-15 14:24:32 +0100

    update

--- Java script/HigherFunctions.js
+++ Java script/HigherFunctions.js
@@ -1,21 +1,36 @@
 const numbers = [3,5,6,4,7,8,42,22,11]
 
-const minNum = numbers.reduce ((accumulator, next) =>
-     Math.min(accumulator, next));
-
-console.log (minNum);
-
+//finds highest and lowerst number
 
 const minNum = numbers.reduce ((accumulator, next) =>
      Math.min(accumulator, next));
 
 console.log (minNum);
 
+// console.log(numbers.reduce((selectedNumber,maxNumber) => selectedNumber > maxNumber ? selectedNumber : maxNumber, 0));
 
+const minNum = numbers.reduce ((accumulator, next) =>
+     Math.min(accumulator, next));
+
+console.log (minNum);
+
+//console.log(numbers.reduce((selectedNumber,maxNumber) => selectedNumber < maxNumber ? selectedNumber : maxNumber, Infinity));
+
+//filters odd and even
 const filterONum = numbers.filter ((n) =>
 n % 2==1);
-console.log (filterNum)
+console.log (filterONum)
 
 const filterENum = numbers.filter ((n) =>
-n % 2==1);
-console.log (filterNum)
+n % 2==0);
+console.log (filterENum)
+
+
+//finds sum total
+
+const totalNum = numbers.reduce ((acc, next) => acc+next);
+console.log (totalNum);
+
+
+const square = numbers.maps(num => num * num).filter(num=> num %2 == 0).reduce((accumulator,next) => Math.min(accumulator, next));
+console.log (square)
