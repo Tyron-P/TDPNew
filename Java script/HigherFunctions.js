214c81da2c0fd095ff23e9cca6f9c6bceba9703f
Tyron Pyle <tyron.pyle@gmail.com>
2022-06-15 12:33:59 +0100

    js update

---
+++ Java script/HigherFunctions.js
@@ -0,0 +1,21 @@
+const numbers = [3,5,6,4,7,8,42,22,11]
+
+const minNum = numbers.reduce ((accumulator, next) =>
+     Math.min(accumulator, next));
+
+console.log (minNum);
+
+
+const minNum = numbers.reduce ((accumulator, next) =>
+     Math.min(accumulator, next));
+
+console.log (minNum);
+
+
+const filterNum = numbers.filter ((n) =>
+n % 2==1);
+console.log (filterNum)
+
+const filterNum = numbers.filter ((n) =>
+n % 2==1);
+console.log (filterNum)
