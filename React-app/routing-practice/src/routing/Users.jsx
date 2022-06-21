c40a6181918065a4ecb9f4946dd64f8c38e6afe4
Tyron Pyle <tyron.pyle@gmail.com>
2022-06-21 13:06:42 +0100

    update working

--- React-app/routing-practice/src/routing/Users.jsx
+++ React-app/routing-practice/src/routing/Users.jsx
@@ -9,13 +9,13 @@ return (
     <strong>Select a user:</strong>
              <ul>
                  <li>
-                     <Link to="/users/1">User 1</Link>
+                     <Link to="/users/Tyron">User 1</Link>
                  </li>
                  <li>
-                     <Link to="/users/2">User 2</Link>
+                     <Link to="/users/Niki">User 2</Link>
                  </li>
                  <li>
-                     <Link to="/users/3">User 3</Link>
+                     <Link to="/users/Michal">User 3</Link>
                  </li>
              </ul>
              <p>{id}</p>
